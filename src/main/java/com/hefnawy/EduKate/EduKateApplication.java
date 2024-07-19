6a8860821fd6a126708f657f213de7030b617ce7
Hefnawy <Mohamed.elhefnawy@fawry.com>
2024-07-19 18:26:25 +0300

    Initial

---
+++ src/main/java/com/hefnawy/EduKate/EduKateApplication.java
@@ -0,0 +1,13 @@
+package com.hefnawy.EduKate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduKateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduKateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hefnawy/EduKate/EduKateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hefnawy.EduKate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduKateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
